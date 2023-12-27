bada9b26fe44ab4e6ea4271ca198e86885dc3c96
Andreas Kurz <off.andreaskurz+github@gmail.com>
2023-12-27 01:25:19 +0100

    finished day 5 with a very optimised solution

--- day05.hs
+++ day05.hs
@@ -1,18 +1,17 @@
 module Main where
 import Util (splitString)
+import Debug.Trace
 
 main :: IO ()
 main = do
-    contents <- readFile "day05.ex.data"
+    contents <- readFile "day05.data"
     
     let plantation = parsePlantation contents
     let locations = getEndLocations plantation
     print $ minimum locations
     
     let locations = getEndLocations2 plantation
-    print $ minimum locations
-
-
+    print $ minimum $ map fst locations
 
 type Map = [(Integer, Integer, Integer)]
 
@@ -78,33 +77,39 @@ getEndLocations (Plantation seeds soil fertilizer water light temperature humidi
         seeds
 
 -- part 2
-rangeFromLen :: Integer -> Integer -> [Integer]
-rangeFromLen i1 i2 = [x | x <- [i1..(i1 + i2)]]
-
 zipInPlace :: [Integer] -> [(Integer, Integer)]
 zipInPlace [] = []
 zipInPlace [a] = []
 zipInPlace (i1 : i2 : rest) = (i1, i2) : zipInPlace rest
 
-
-
-getEndLocations2 :: Plantation -> [Integer]
+getEndLocations2 :: Plantation -> [(Integer, Integer)]
 getEndLocations2 (Plantation seeds soil fertilizer water light temperature humidity location) =
     let
-        applyMap :: Map -> Integer -> Integer
-        applyMap [] seed = seed
-        applyMap ((dstart, sstart, rlen) : rest) seed
-            | sstart <= seed && seed < (sstart + rlen) = dstart + seed - sstart
-            | otherwise = applyMap rest seed
+        applyMap :: Map -> (Integer, Integer) -> [(Integer, Integer)]
+        applyMap [] seed = [seed]
+        applyMap ((dstart, sstart, rlen) : rest) (seedStart, seedEnd)
+            | seedEnd < sstart = applyMap rest (seedStart, seedEnd)
+            | seedStart >= (sstart + rlen) = applyMap rest (seedStart, seedEnd)
+            | seedStart >= sstart && seedEnd < (sstart + rlen) = [(dstart + seedStart - sstart, dstart + seedEnd - sstart)]
+            | seedStart >= sstart = 
+                    (dstart + seedStart - sstart, dstart + rlen) : 
+                    applyMap rest (sstart + rlen, seedEnd)
+            | seedEnd < (sstart + rlen) = 
+                    (dstart, dstart + seedEnd - sstart) : 
+                    applyMap rest (seedStart, sstart - 1)
+            | seedStart < sstart && seedEnd >= (sstart + rlen) = 
+                    (dstart, dstart + rlen) : 
+                    (applyMap rest (seedStart, sstart - 1) ++ applyMap rest (sstart + rlen, seedEnd))
+            | otherwise = error ("should not happen " ++ show (dstart, sstart, rlen) ++ " " ++ show (seedStart, seedEnd))
     in
 
-    map (applyMap location) 
-        $ map (applyMap humidity) 
-        $ map (applyMap temperature) 
-        $ map (applyMap light)
-        $ map (applyMap water)
-        $ map (applyMap fertilizer)
-        $ map (applyMap soil)
-        $ concatMap (\(i1, i2) -> rangeFromLen i1 i2) $ zipInPlace seeds
+    concatMap (applyMap location) 
+        $ concatMap (applyMap humidity) 
+        $ concatMap (applyMap temperature) 
+        $ concatMap (applyMap light)
+        $ concatMap (applyMap water)
+        $ concatMap (applyMap fertilizer)
+        $ concatMap (applyMap soil)
+        $ map (\(i1, i2) -> (i1, i1 + i2)) $ zipInPlace seeds
 
 
